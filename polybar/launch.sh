1d68a7ee92886092a65303e7f3b73735cd5366ec
Simax <simaxme@gmail.com>
2022-07-26 21:16:58 +0200

    excluded rofi from picom

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,3 +1,11 @@
-echo "---" | tee -a /tmp/polybar1.log
-polybar 2>&1 | tee -a /tmp/polybar1.log & disown
-echo "Bars launched..."
+# echo "---" | tee -a /tmp/polybar1.log
+# polybar 2>&1 | tee -a /tmp/polybar1.log & disown
+# echo "Bars launched..."
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload &
+  done
+else
+  polybar --reload example &
+fi
