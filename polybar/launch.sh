d4f56237b6ff1e50c0669a354eb14cc1c71c0089
Simax <simaxme@gmail.com>
2022-08-10 21:04:33 +0200

    update

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -2,6 +2,8 @@
 # polybar 2>&1 | tee -a /tmp/polybar1.log & disown
 # echo "Bars launched..."
 
+killall polybar
+
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
     MONITOR=$m polybar --reload &
