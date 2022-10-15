48cff66fe4ce798f75582955a28464ac4e0a7ed7
Simax <simaxme@gmail.com>
2022-10-15 13:11:07 +0200

    config update

---
+++ i3/resolution.sh
@@ -0,0 +1,4 @@
+xrandr --output HDMI-2 --off
+xrandr --output DP-1 --mode 3840x1080 --rate 120
+xrandr --setmonitor DP-1-1 2560/800x1080/340+0+0 DP-1
+xrandr --setmonitor DP-1-2 1280/400x1080/340+2560+0 none
--- polybar/launch.sh
+++ polybar/launch.sh
@@ -2,19 +2,37 @@
 # polybar 2>&1 | tee -a /tmp/polybar1.log & disown
 # echo "Bars launched..."
 
+IS_SUPER_ULTRAWIDE_MONITOR=true
+VIRTUAL_SCREEN_NAMES=("DP-1-1" "DP-1-2")
+
 killall polybar
 
 FIRST_TIME=true
 
-if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    if [ "$FIRST_TIME" = true ]; then
-      FIRST_TIME=false
-      MONITOR=$m TRAY_POSITION=right polybar default --reload &
-    else
-      MONITOR=$m TRAY_POSITION=none polybar default --reload &
-    fi
-  done
+if [ "$IS_SUPER_ULTRAWIDE_MONITOR" = true ]; then
+    for m in ${VIRTUAL_SCREEN_NAMES[@]}; do 
+        if [ "$FIRST_TIME" = true ]; then
+          FIRST_TIME=false
+          MONITOR=$m TRAY_POSITION=right polybar default --reload &
+        else
+          MONITOR=$m TRAY_POSITION=none polybar default --reload &
+        fi
+    done
 else
-  polybar --reload example &
+    if type "xrandr"; then
+      for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+        if [ "$FIRST_TIME" = true ]; then
+          FIRST_TIME=false
+          MONITOR=$m TRAY_POSITION=right polybar default --reload &
+        else
+          MONITOR=$m TRAY_POSITION=none polybar default --reload &
+        fi
+      done
+    else
+      polybar --reload example &
+    fi
 fi
+
+
+
+
