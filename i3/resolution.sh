57d722be55fb23683f68ccc64682e4d68867ba4d
Simax <simaxme@gmail.com>
2022-10-17 19:41:46 +0200

    resolution update

--- i3/resolution.sh
+++ i3/resolution.sh
@@ -1,4 +1,14 @@
-xrandr --output HDMI-2 --off
-xrandr --output DP-1 --mode 3840x1080 --rate 120
-xrandr --setmonitor DP-1-1 2560/800x1080/340+0+0 DP-1
-xrandr --setmonitor DP-1-2 1280/400x1080/340+2560+0 none
+MAKE_CUSTOM_RESOLUTION=false
+
+if [ MAKE_CUSTOM_RESOLUTION = true ] then    
+    xrandr --output HDMI-2 --off
+    xrandr --output DP-1 --mode 3840x1080 --rate 120
+    xrandr --setmonitor DP-1-1 2560/800x1080/340+0+0 DP-1
+    xrandr --setmonitor DP-1-2 1280/400x1080/340+2560+0 none
+else
+    xrandr --output HDMI-2 --mode 1920x1080 --pos 1920x0 --output DP-1 --mode 1920x1080 --pos 0x0
+fi
+
+sleep 3
+
+nitrogen --restore
