8f1c9cd84bd532046ff817d6781067d16f84ddfd
Simax <simaxme@gmail.com>
2022-12-24 22:26:34 +0100

    update

--- awesome/themes/dracula/theme.lua
+++ awesome/themes/dracula/theme.lua
@@ -15,7 +15,7 @@ local palette = require('themes.dracula.palette')
 
 local gears = require('gears')
 
-theme.font          = "CaskaydiaCove Nerd Font Regular 10"
+theme.font          = "CaskaydiaCove Nerd Font Regular 11"
 
 theme.bg_normal     = "#222222"
 theme.bg_focus      = "#535d6c"
