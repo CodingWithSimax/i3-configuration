f781f3fc7dd6c59d175cf072927e87963a13d534
Simax <simaxme@gmail.com>
2023-03-17 21:59:55 +0100

    update

--- awesome/resolution.sh
+++ awesome/resolution.sh
@@ -1,10 +1,10 @@
-MAKE_CUSTOM_RESOLUTION=false
+MAKE_CUSTOM_RESOLUTION=true
 
-if [ MAKE_CUSTOM_RESOLUTION = true ]; then    
-    xrandr --output HDMI-2 --off
-    xrandr --output DP-1 --mode 3840x1080 --rate 120
-    xrandr --setmonitor DP-1-1 2560/800x1080/340+0+0 DP-1
-    xrandr --setmonitor DP-1-2 1280/400x1080/340+2560+0 none
-else
-    xrandr --output HDMI-1 --mode 1920x1080 --pos 1920x0 --output DP-0 --mode 1920x1080 --pos 0x0
-fi
+# if [ MAKE_CUSTOM_RESOLUTION = true ]; then    
+xrandr --output HDMI-1 --off
+xrandr --output DP-0 --mode 3840x1080 --rate 120
+xrandr --setmonitor DP-1-1 1920/600x1080/340+0+0 DP-0
+xrandr --setmonitor DP-1-2 1920/600x1080/340+1920+0 none
+# else
+    # xrandr --output HDMI-1 --mode 1920x1080 --pos 1920x0 --output DP-0 --mode 1920x1080 --pos 0x0
+# fi
